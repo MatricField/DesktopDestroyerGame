e01c18328a5692b3eda57ec533f96fb4a967a7bd
Lucien Du <david.xelnaga@gmail.com>
2018-06-17 18:19:08 -0700

    Proof of concept

---
+++ DesktopDestroyer/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopDestroyer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly RoutedCommand CmdStartGame = new RoutedCommand();
+
+        private GameState gameState;
+
+        public MainWindow()
+        {
+            gameState = GameState.WelcomeScreen;
+            InitializeComponent();
+        }
+
+        private async void StartGame(object sender, ExecutedRoutedEventArgs e)
+        {
+            Hide();
+            await Task.Delay(200);
+            var nextPage = new PlayfieldPage();
+            Content = nextPage;
+            WindowStyle = WindowStyle.None;
+            WindowState = WindowState.Maximized;
+            Show();
+            gameState = GameState.Started;
+        }
+
+        private void CanStartGame(object sender, CanExecuteRoutedEventArgs e)
+        {
+            switch(gameState)
+            {
+                case GameState.WelcomeScreen:
+                    e.CanExecute = true;
+                    break;
+                default:
+                    e.CanExecute = false;
+                    break;
+            }
+        }
+
+        private enum GameState
+        {
+            WelcomeScreen,
+            Started
+        }
+    }
+}
---
+++ DesktopDestroyer/PlayfieldPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopDestroyer
+{
+    /// <summary>
+    /// Interaction logic for PlayfieldPage.xaml
+    /// </summary>
+    public partial class PlayfieldPage : Page
+    {
+        public BitmapSource BackgroundImage
+        {
+            get { return (BitmapSource)GetValue(BackgroundImageProperty); }
+            set { SetValue(BackgroundImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BackgroundImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackgroundImageProperty =
+            DependencyProperty.Register(
+                "BackgroundImage",
+                typeof(BitmapSource),
+                typeof(PlayfieldPage),
+                new PropertyMetadata(null));
+
+
+        public PlayfieldPage()
+        {
+            BackgroundImage = ScreenCaptureHelper.CaptureScreen();
+            InitializeComponent();
+        }
+
+        private void DrawingBoard_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var mousePos = Mouse.GetPosition(DrawingBoard);
+            var shape = new Ellipse()
+            {
+                Fill = new SolidColorBrush(Colors.Black),
+                Width = 50,
+                Height = 50,
+            };
+            Canvas.SetLeft(shape, mousePos.X);
+            Canvas.SetTop(shape, mousePos.Y);
+            DrawingBoard.Children.Add(shape);
+        }
+    }
+}
